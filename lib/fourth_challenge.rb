f6c2b4f11cbac3aecada7d7bf895164a585525d9
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-10 12:10:16 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,8 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age:"17", status: "worried"},
-        {name: "Mercutio", age:"18", status: "hot-headed"}
+        {name: "Benvolio", age:"17", attitude: "worried"},
+        {name: "Mercutio", age:"18", attitude: "hot-headed"}
         
       ]
    }, 
@@ -15,7 +15,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {name: "Benvolio", age:"17", attitude: "worried"},
+        {name: "Mercutio", age:"18", attitude: "hot-headed"}
       ]
    }
 }
