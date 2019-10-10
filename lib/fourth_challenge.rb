d687f4cf6f7e243a83803577c9ac2518ca7f596f
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-10 12:09:31 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         {name: "Benvolio", age:"17", status: "worried"},
-        #your hashes here!
+        {name: "Mercutio", age:"18", status: "hot-headed"}
+        
       ]
    }, 
    :capulet => {
